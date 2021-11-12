6e77862c5b5be8beaa05000489cbf8c84d77b3eb
Andy Smith <work@andysmith.is>
2021-11-12 11:03:38 +1100

    added new server

--- pages/api/footer/index.js
+++ pages/api/footer/index.js
@@ -1,11 +1,7 @@
+import { getSomething } from '@/lib/api';
+
 export default async function handler(req, res) {
-  const result = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/footer-nav`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log('footer result', result);
-  const footer = await result.json();
+  const footer = await getSomething('footer-nav');
+
   res.status(200).json({ footer });
 }
--- pages/api/navigation/index.js
+++ pages/api/navigation/index.js
@@ -1,11 +1,6 @@
+import { getSomething } from '@/lib/api';
+
 export default async function handler(req, res) {
-  const result = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/top-nav-menu`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log('nav result', result);
-  const nav = await result.json();
+  const nav = await getSomething('top-nav-menu');
   res.status(200).json({ nav });
 }
